e32e6365c7e576ab65b65954019c9e45def4ae92
cree02 <chris.heitchue@gmail.com>
2026-05-12 23:57:36 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'hub-v1';
+const PRECACHE = [
+  '/',
+  '/index.html',
+  '/dashboards/ledger.html',
+  '/dashboards/one-on-one-hub.html',
+  '/dashboards/first-101.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(PRECACHE)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
